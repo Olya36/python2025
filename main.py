5213e463f7aaa818b8a058d2e832b2827bdffe8b
megamarket <markina_o@1060.ru>
2025-03-03 17:09:11 +0300

    Запрос к телеграм API с помощью библиотеки requests

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+from config import TOKEN
+
+
+base_url = f'https://api.telegram.org/bot{TOKEN}/'
+command = 'getMe'
+
+response = requests.get(base_url + command)
+
+print(response.json())
