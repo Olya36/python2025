fd1b1b7fff4f8f81f9a10a6034a9d947f6dbb42c
megamarket <markina_o@1060.ru>
2025-03-03 17:08:06 +0300

    Простой бот на библиотеке python-telegram-bot с логированием через библиотеку logging

---
+++ bot.py
@@ -0,0 +1,45 @@
+import logging
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+from config import TOKEN
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(astime)s | %(levelname)s | %(name)+30s [%(lineno)4d] - %(message)s'
+)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """функция, которая будет на команды / hello"""
+    user = update.effective_user
+    log.info(f'Функция hello вызвана пользователем {user}')
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """функция, которая реагтрует эхом на любое сообщение"""
+    user = update.effective_user
+    message = update.message
+    log.info(
+        f'Функция echo вызвана пользователем {user}\n'
+        f'{message = }'
+    )
+    await update.message.reply_text(f'{update.message.text}')
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+
+app.add_handler(CommandHandler("hello", hello)) #регистрация обработчика
+app.add_handler(MessageHandler(filters.ALL, echo))
+
+app.run_polling()
+
+
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
