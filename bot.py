7d2ddf84fe4d18f1fa0a04f41f9b2c1ac97a6601
megamarket <markina_o@1060.ru>
2025-03-24 15:19:44 +0300

    Реакции на клавиатуре

--- bot.py
+++ bot.py
@@ -1,45 +1,163 @@
 import logging
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, CallbackQueryHandler, filters
 
 from config import TOKEN
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(astime)s | %(levelname)s | %(name)+30s [%(lineno)4d] - %(message)s'
+    format='%(asctime)s | %(levelname)s | %(module)-15s [%(lineno)4d] - %(message)s'
 )
 logging.getLogger('httpx').setLevel(logging.WARNING)
 log = logging.getLogger(__name__)
 
+
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """функция, которая будет на команды / hello"""
+    """функция, которая будет реагировать на команду /hello"""
     user = update.effective_user
     log.info(f'Функция hello вызвана пользователем {user}')
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """функция, которая реагтрует эхом на любое сообщение"""
+    """функция, которая реагирует эхом на любое сообщение"""
     user = update.effective_user
     message = update.message
-    log.info(
-        f'Функция echo вызвана пользователем {user}\n'
-        f'{message = }'
-    )
+    log.info(f'Функция echo вызвана пользователем {user}\n' + ' ' * 73 + f'{message = }')
     await update.message.reply_text(f'{update.message.text}')
 
 
+async def say_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Рассказывает, какие есть возможности у бота"""
+    user = update.effective_user
+    log.info(f'Функция help вызвана пользователем {user}\n')
+
+    text = [
+        f'Привет, {user.first_name}!',
+        '',
+        'Я - школьный бот, который умеет реагировать на следующие команды:',
+        '/hello - отвечаю приветствием',
+        '/start /help - покажу список доступных команд',
+        '/keyboard - покажу клавиатуру',
+    ]
+    text = '\n'.join(text)
+
+    await update.message.reply_text(text)
+
+
+async def say_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Показывает inline-клавиатуру"""
+    user = update.effective_user
+    log.info(f'Функция say_keyboard вызвана пользователем {user}')
+
+    buttons = [
+        [InlineKeyboardButton('Раз', callback_data='Раз'),
+         InlineKeyboardButton('Два', callback_data='Два'),
+         InlineKeyboardButton('Три', callback_data='Три')],
+        [InlineKeyboardButton('Четыре', callback_data='Четыре'),
+         InlineKeyboardButton('Пять', callback_data='Пять')],
+        [InlineKeyboardButton('Шесть', callback_data='Шесть')]
+    ]
+    keyboard = InlineKeyboardMarkup(buttons)
+
+    await update.message.reply_text(
+        text='Выберите опцию на клавиатуре',
+        reply_markup=keyboard
+    )
+
+
+async def react_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Реагирует на нажатие кнопки на inline-клавиатуре"""
+    user = update.effective_user
+    log.info(f'Функция react_keyboard вызвана пользователем {user}')
+
+    query = update.callback_query
+
+    if query.data == 'Раз':
+        return await one(update, context)
+    elif query.data == 'Два':
+        return await two(update, context)
+    elif query.data == 'Три':
+        return await three(update, context)
+    elif query.data == 'Четыре':
+        return await four(update, context)
+
+    buttons = [
+        [InlineKeyboardButton('Раз', callback_data='Раз'),
+         InlineKeyboardButton('Два', callback_data='Два'),
+         InlineKeyboardButton('Три', callback_data='Три')],
+        [InlineKeyboardButton('Четыре', callback_data='Четыре'),
+         InlineKeyboardButton('Пять', callback_data='Пять')],
+        [InlineKeyboardButton('Шесть', callback_data='Шесть')]
+    ]
+    keyboard = InlineKeyboardMarkup(buttons)
+
+    await query.edit_message_text(
+        text=query.data,
+        reply_markup=keyboard
+    )
+
+async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Выведет ответ на команду 'Раз'"""
+    user = update.effective_user
+    log.info(f'Функция one вызвана пользователем {user}\n')
+
+    query = update.callback_query
+
+    text = 'Машина'
+    await query.edit_message_text(
+        text=text
+    )
+
+
+async def two(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Выведет ответ на команду 'Два'"""
+    user = update.effective_user
+    log.info(f'Функция two вызвана пользователем {user}\n')
+
+    query = update.callback_query
+
+    text = 'Инторматика'
+    await query.edit_message_text(
+        text=text
+    )
+
+
+async def three(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Выведет ответ на команду 'Три'"""
+    user = update.effective_user
+    log.info(f'Функция three вызвана пользователем {user}\n')
+
+    query = update.callback_query
+
+    text = 'Мотоцикл'
+    await query.edit_message_text(
+        text=text
+    )
+
+
+async def four(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Выведет ответ на команду 'Четыре'"""
+    user = update.effective_user
+    log.info(f'Функция four вызвана пользователем {user}\n')
+
+    query = update.callback_query
+
+    text = 'Кот'
+    await query.edit_message_text(
+        text=text
+    )
+
 app = ApplicationBuilder().token(TOKEN).build()
 
-
-app.add_handler(CommandHandler("hello", hello)) #регистрация обработчика
-app.add_handler(MessageHandler(filters.ALL, echo))
-
-app.run_polling()
-
-
-
+# регистрация обработчиков
 app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler(["help", "start"], say_help))
+app.add_handler(CommandHandler("keyboard", say_keyboard))
+app.add_handler(CallbackQueryHandler(react_keyboard))
+app.add_handler(MessageHandler(filters.ALL, echo))
 
 app.run_polling()
