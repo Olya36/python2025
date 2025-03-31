4d12d17cd2523495da3279d7d5b1edcf83c06400
megamarket <markina_o@1060.ru>
2025-03-31 15:24:38 +0300

    Добавили зарисовки функции для бота световиков

---
+++ events.py
@@ -0,0 +1,25 @@
+
+
+from telegram import Update
+from telegram.ext import ContextTyper
+async def ask_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+async def ask_place(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+
+async def ask_equipment(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+
+async def ask_person(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
+
+
+
+async def ask_check(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    pass
